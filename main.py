a825772a427970d920dc4365c621b4d04abacad0
Alona Praslov <alona@praslov.com>
2025-06-26 18:38:14 +0300

    updated output format

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import sys
+from stats import get_num_words
+
 def main():
-    book_path = "books/frankenstein.txt"
+    # book_path = "books/frankenstein.txt"
+
+    if len(sys.argv) <= 1:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_char = get_num_char(text)
@@ -15,13 +25,9 @@ def main():
     print("")
     
     for key in num_char:
-        print(f"The '{key}' character was found {num_char[key]} times")
+        print(f"{key}: {num_char[key]}")
 
     print("--- End report ---")
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
 
 
 def get_book_text(path):
